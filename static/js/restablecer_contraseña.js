e10ee3599d89b9750e79da62abcf831768570b62
AlexDiaz006 <166964894+AlexDiaz006@users.noreply.github.com>
2025-04-24 01:00:09 -0300

    Update restablecer_contraseña.js

--- static/js/restablecer_contraseña.js
+++ static/js/restablecer_contraseña.js
@@ -1,15 +1,21 @@
-function validatePasswords() {
-    const newPassword = document.getElementById('new-password').value;
-    const confirmPassword = document.getElementById('confirm-password').value;
+function validateFields() {
+    const dni = document.getElementById('dni').value;
+    const email = document.getElementById('email').value;
 
-    if (!newPassword || !confirmPassword) {
+    if (!dni || !email) {
         alert('Ambos campos deben estar llenos. Por favor, inténtelo de nuevo.');
         return;
     }
 
-    if (newPassword !== confirmPassword) {
-        alert('Las contraseñas no coinciden. Por favor, inténtelo de nuevo.');
-    } else {
-        alert('Contraseña cambiada con éxito.');
+    if (!/^\d+$/.test(dni)) {
+        alert('El DNI debe contener solo números. Por favor, inténtelo de nuevo.');
+        return;
     }
+
+    if (!/^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email)) {
+        alert('Ingrese un correo electrónico válido.');
+        return;
+    }
+
+    alert('Datos enviados correctamente.');
 }
